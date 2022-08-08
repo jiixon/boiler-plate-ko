266244fd0e1bc4e9633e8f7426c4badd37f73bc4
jiwon In <9430804@naver.com>
2022-08-08 19:51:46 +0900

    로그아웃기능 추가

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser'); 
+const { User } = require("./models/User");
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 
 const { auth } = require('./middleware/auth');
-const { User } = require("./models/User");
 
 //body-Parser가 client에서 오는 정보를 서버에서 분석하여 가져오도록 하기
 //application/x-www-form-urlencoded
@@ -28,10 +28,15 @@ mongoose.connect(config.mongoURI, {   //config.mongoURI(비밀)
 
 app.get('/', (req, res) => {res.send('Hello World! WOW~')})
 
+app.post('/api/users/register', (req,res) => {
+
+})
 app.post('/api/users/register', (req,res) => {  //post 매소드이용
 
   //회원 가입 할때 필요한 정보들을 client에서 가져오면
   //그것들을 데이터 베이스에 넣어준다. (5번째 줄)
+
+
 
   const user = new User(req.body) 
   //모든정보들을 모델에 넣어주고, user.save를 하기전에, 비밀번호 암호화가 필요(User.js)
@@ -42,6 +47,10 @@ app.post('/api/users/register', (req,res) => {  //post 매소드이용
       success: true  //성공시, 이 문장 출력하도록
     })
   }) 
+})
+app.post('/api/users/register',(req, res) => {
+  const user = new User(req.body)
+  
 })
 
 app.post('/api/users/login', (req, res) => {
@@ -91,6 +100,18 @@ app.get('/api/users/auth', auth ,(req,res) => {
     role: req.user.role,
     image: req.user.image
   })
+})
+
+//로그아웃 route
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({_id: req.user._id}, //DB에서 찾을 건데, auth미들웨어에서 id로 찾겠다.
+    {token: ""} //토큰을 지워준다.
+    , (err, user) => { 
+      if (err) return res.json({ success: false, err});
+      return res.status(200).send({ //찾는데 성공하면,
+        success: true
+      })
+    })
 })
 
 app.listen(port, () => {console.log(`Example app listening on port ${port}`)})
